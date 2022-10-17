40ef9572bb3bee73a4570208a10d5e5a59717173
+ <+>
2022-10-17 12:57:48 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,20 @@
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+puts "Hello World!"
+
+# app.rb
+print "Hello World!"
+print "Hello World!"
+print "Hello World!"
+
+#app.rb
+puts [1,2,3,4]
+p [1,2,3,4]
+
+# app.rb
+pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+print "Pass this test, please."
+
+p [1,2,3]
